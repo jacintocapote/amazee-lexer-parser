3354fd9b2d15912f4f89847275b97622e3c44ab1
Jacinto Capote Robles <jacintorobles@appnovation.com>
2019-05-06 21:30:42 +0200

    Updated script install

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,7 +5,7 @@ echo "Executing composer install"
 # Running 'composer install' to keep the folder structure and content up to
 # date with the current branch.
 ###
-docker-compose exec cli
+docker-compose exec cli composer install
 echo ""
 echo "Installing amazee lexer parser"
 docker-compose exec cli drush si:install --account-name=amazee --account-pass=test --site-name="Amazee Lexer & Parser" --site-mail="jacintocapote@gmail.com" --existing-config --yes --verbose
